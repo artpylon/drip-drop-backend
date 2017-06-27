294578fac8a59ebe1eb63639109832bb727f87ee
rothenbg <geoffreyrothenberg@gmail.com>
2017-06-27 10:33:05 -0400

    Create controller, change express-api-template, create routes
    Create crude controller from examples.js template, swap out
    'express-api' for 'drip-drop-backend', create CRUD routes

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const debug = require('debug')('express-api-template:users')
+const debug = require('debug')('drip-drop-backend:users')
 
 const controller = require('lib/wiring/controller')
 const models = require('app/models')
