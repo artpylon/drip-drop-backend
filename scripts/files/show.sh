e0c72965b49130b71a779a71a5b55eeb0d7eb78b
MarcyNash <marcywsa@gmail.com>
2017-06-28 20:49:23 -0400

    CURL scripts for files - show.sh and update.sh
    Show script works. Still getting the 404 with the update

---
+++ scripts/files/show.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+API="http://localhost:4741"
+URL_PATH="/files"
+
+curl "${API}${URL_PATH}/${ID}" \
+  --include \
+  --request GET \
+
+echo
+
+
+
+# db.files.findOne({_id : ObjectId("5953f97544ff1208a4f83c69")})
+# {
+# 	"_id" : ObjectId("5953f97544ff1208a4f83c69"),
+# 	"_owner" : "5952d923ced6a7a1f1e51eb8",
+# 	"name" : "junk",
+# 	"folder" : "3myfolder",
+# 	"url" : "abc",
+# 	"updatedAt" : ISODate("2017-06-28T19:41:44.739Z")
+# }
